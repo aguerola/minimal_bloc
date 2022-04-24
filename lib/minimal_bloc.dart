b8ad1c333997309dd4de0b8d9633ba68681c9595
Antonio Martínez Guerola <amg1983@gmail.com>
2022-04-24 10:37:04 +0200

    updated exports

--- lib/minimal_bloc.dart
+++ lib/minimal_bloc.dart
@@ -1,7 +1,4 @@
 library minimal_bloc;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'stateful_bloc_widget.dart';
+export 'bloc.dart';
